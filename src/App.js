88f9be727b9e14cafae6b65453ff8a7295cd1ba8
DesignerBloom <sandeepblooms@gmail.com>
2024-09-19 15:59:23 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 //import logo from './logo.svg';
 import './App.css';
 import Grid from './Table/DataGrid';
+import FullFeaturedCrudGrid from './Table/TableEditing';
 
 function App() {
   return (
     <div className="App">
-      <Grid />
+      {/* <Grid /> */}
+      <FullFeaturedCrudGrid />
     </div>
   );
 }
